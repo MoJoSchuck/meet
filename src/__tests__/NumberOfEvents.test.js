7c3b560c864c096b85d557e345ddd591da29f53e
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-06-19 11:35:36 +0200

    Create NumberOfEvents.test.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,24 @@
+// src/__tests__/NumberOfEvents.test.js
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents';
+
+test('renders textbox for NumberOfEvents component', () => {
+  render(<NumberOfEvents />);
+  const inputElement = screen.getByRole('textbox');
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('default value of the textbox is 32', () => {
+  render(<NumberOfEvents />);
+  const inputElement = screen.getByRole('textbox');
+  expect(inputElement).toHaveValue(32);
+});
+
+test('change value of the textbox when user types', async () => {
+  const user = userEvent.setup();
+  render(<NumberOfEvents />);
+  const inputElement = screen.getByRole('textbox');
+  await user.type(inputElement, '{backspace}{backspace}10');
+  expect(inputElement).toHaveValue(10);
+});
