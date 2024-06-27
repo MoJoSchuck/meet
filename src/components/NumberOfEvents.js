c5c837179b277cfa7116552910526fe491c912c1
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-06-27 17:00:05 +0200

    Update NumberOfEvents.js

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,27 +1,37 @@
 // src/components/NumberOfEvents.js
 import React, { useState } from 'react';
 
-const NumberOfEvents = ({ setCurrentNOE }) => {
-  const [numberOfEvents, setNumberOfEvents] = useState(32);
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+    const [numberOfEvents, setNumberOfEvents] = useState(32);
 
-  const handleInputChanged = (event) => {
-    const value = Number(event.target.value);
-    setNumberOfEvents(value);
-    setCurrentNOE(value); // Props verwenden
-  };
+    const handleInputChanged = (event) => {
+        const value = Number(event.target.value);
+        setNumberOfEvents(value);
+        setCurrentNOE(value); // Props verwenden
 
-  return (
-    <div id="number-of-events">
-      <label htmlFor="number-of-events-input">Number of Events: </label>
-      <input
-        id="number-of-events-input"
-        type="number"
-        value={numberOfEvents}
-        onChange={handleInputChanged}
-        role="textbox"
-      />
-    </div>
-  );
+        let numberText;
+        if (isNaN(event.target.value) || event.target.value <= 0) {
+            numberText = "Only positive numbers are allowed"
+        }
+
+        else {
+            numberText = ""
+        }
+        setErrorAlert(numberText);
+    };
+
+    return (
+        <div id="number-of-events">
+            <label htmlFor="number-of-events-input">Number of Events: </label>
+            <input
+                id="number-of-events-input"
+                type="number"
+                value={numberOfEvents}
+                onChange={handleInputChanged}
+                role="textbox"
+            />
+        </div>
+    );
 };
 
 export default NumberOfEvents;
