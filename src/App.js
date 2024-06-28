160456e80e2d8e9ef23d6020b5fd0873f9011067
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-06-28 14:53:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,6 +31,8 @@ const App = () => {
 
     return (
         <div className="App">
+            <h1>Meet App</h1>
+            <h3>Choose your nearest city</h3>
             <div className="alerts-container">
                 {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
                 {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
